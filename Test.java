8d31a840c12fa8ba34583ee006fe329f4cd356eb
hanzaliv <wahansaliviru@gmail.com>
2025-02-05 13:23:51 +0530

    age is updated

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test{
  public static void main(String[] args){
 	String name = "John";
 	System.out.println("Hello " + name);
+	int age = 20;
+	System.out.println("You are " + age + " years old");
 	}
 }
