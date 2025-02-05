3a903b399d8cf99b20bc46eb8b5bbb0372172899
hanzaliv <wahansaliviru@gmail.com>
2025-02-05 14:11:47 +0530

    Test and other files added

--- Test.java
+++ Test.java
@@ -4,5 +4,6 @@ public class Test{
 	System.out.println("Hello " + name);
 	int age = 20;
 	System.out.println("You are " + age + " years old");
+	System.out.println("Your name is " + name + " and you are " + age + " years old");
 	}
 }
